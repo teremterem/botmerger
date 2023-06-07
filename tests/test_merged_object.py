1f50897bfcaba4fe7229c4ecf19356a28b3a270c
Oleksandr Tereshchenko <toporok@gmail.com>
2023-06-07 09:46:55 +0300

    botmerger

--- tests/test_merged_object.py
+++ tests/test_merged_object.py
@@ -1,24 +1,14 @@
 """Tests for the `MergedObject` class."""
-from mergedbots.botmerger.base import BotMerger, MergedObject
-from mergedbots.botmerger.models import MergedBot
+import pytest
+from pydantic import ValidationError
+
+from mergedbots.botmerger.base import MergedObject
+from mergedbots.botmerger.core import InMemoryBotMerger
 
 
 def test_merged_object() -> None:
     """Test the `MergedObject` class."""
-
-    class TestMerger(BotMerger):
-        """A test merger."""
-
-        def create_bot(self, alias: str, name: str = None, **kwargs) -> MergedBot:
-            """Create a merged bot while outside an async context."""
-
-        async def create_bot_async(self, alias: str, name: str = None, **kwargs) -> MergedBot:
-            """Create a merged bot while inside an async context."""
-
-        async def find_bot(self, alias: str) -> MergedBot:
-            """Fetch a bot by its alias."""
-
-    merger = TestMerger()
+    merger = InMemoryBotMerger()
     obj1 = MergedObject(merger=merger)
     obj2 = MergedObject(merger=merger)
     assert obj1 != obj2
@@ -31,3 +21,9 @@ def test_merged_object() -> None:
     assert obj1.extra_fields is not obj2.extra_fields
     # TODO assert obj1.dict() == {"uuid": obj1.uuid, "extra_fields": {}}
     assert MergedObject(merger=merger, extra_fields={"test": "test"}).extra_fields == {"test": "test"}
+
+
+def test_merged_object_without_merger() -> None:
+    """Test the `MergedObject` class without a merger."""
+    with pytest.raises(ValidationError):
+        MergedObject()
