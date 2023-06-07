6a338f2e3a5a96244aac56e4edcf5e1c712b5216
Oleksandr Tereshchenko <toporok@gmail.com>
2023-06-07 09:39:50 +0300

    botmerger

--- tests/test_merged_bot.py
+++ tests/test_merged_bot.py
@@ -9,3 +9,12 @@ def test_merged_bot() -> None:
     assert bot.name == "test"
     assert bot.description == "test description"
     assert bot.is_human is False
+
+
+def test_merged_bot_with_name() -> None:
+    """Test the `MergedBot` class with a name."""
+    bot = InMemoryBotMerger().create_bot("test", name="test name")
+    assert bot.alias == "test"
+    assert bot.name == "test name"
+    assert bot.description is None
+    assert bot.is_human is False
