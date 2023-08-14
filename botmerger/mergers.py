50e0ea6fcf84a88ef7a1e553065c5dd6bf672249
Oleksandr Tereshchenko <toporok@gmail.com>
2023-08-14 21:30:34 +0300

    deserialize yaml log

--- botmerger/mergers.py
+++ botmerger/mergers.py
@@ -1,7 +1,9 @@
 """Various concrete implementations of the BotMerger interface."""
 # pylint: disable=no-name-in-module
 from pathlib import Path
+from pprint import pprint
 from typing import Any, Optional, Dict, Union
+from uuid import UUID
 
 import yaml
 from pydantic import UUID4
@@ -48,32 +50,69 @@ class YamlLogBotMerger(InMemoryBotMerger):
     def __init__(self, yaml_log_file: Union[str, Path]) -> None:
         super().__init__()
         self._yaml_log_file = yaml_log_file if isinstance(yaml_log_file, Path) else Path(yaml_log_file)
+        self._non_empty_yaml_log_exists = self._yaml_log_file.exists() and self._yaml_log_file.stat().st_size > 0
         self._yaml_serializer = YamlSerializer()
+
+        if self._non_empty_yaml_log_exists:
+            with self._yaml_log_file.open("r", encoding="utf-8") as file:
+                # TODO TODO TODO
+                for obj in yaml.safe_load_all(file):
+                    print()
+                    pprint(obj)
+                    print()
 
     async def _register_merged_object(self, obj: MergedObject) -> None:
         await super()._register_merged_object(obj)
         serialized_obj = await self._yaml_serializer.serialize(obj)
 
-        append_delimiter = self._yaml_log_file.exists() and self._yaml_log_file.stat().st_size > 0
         with self._yaml_log_file.open("a", encoding="utf-8") as file:
-            if append_delimiter:
+            if self._non_empty_yaml_log_exists:
                 file.write("\n---\n\n")
             yaml.dump(serialized_obj, file, allow_unicode=True, indent=4)
+            self._non_empty_yaml_log_exists = True
 
 
 class YamlSerializer(MergedSerializerVisitor):
     """A YAML serializer for merged objects."""
 
     # TODO is this really YAML specific ? maybe it should be called something else ?
+    # TODO don't list each field individually during deserialization, exclude the special ones (process them
+    #  separately) and then feed the rest into the merged objects in one go
 
     async def serialize_bot(self, obj: MergedBot) -> Dict[str, Any]:
         return self._pre_serialize(obj, include={"uuid", "alias"})
 
+    async def deserialize_bot(self, obj: Dict[str, Any]) -> MergedBot:
+        # TODO when a bot with the same alias is created, make sure to merge it with the loaded one
+        return MergedBot(
+            uuid=UUID(obj["uuid"]),
+            alias=obj["alias"],
+            # TODO this is a hack until I figure out how to merge loaded bots with ones that are being set up
+            name=obj["alias"],
+        )
+
     async def serialize_user(self, obj: MergedUser) -> Dict[str, Any]:
         return self._pre_serialize(obj, exclude={"is_human"})
 
+    async def deserialize_user(self, obj: Dict[str, Any]) -> MergedUser:
+        return MergedUser(uuid=UUID(obj["uuid"]), name=obj["name"])
+
     async def serialize_original_message(self, obj: OriginalMessage) -> Dict[str, Any]:
         return await self._pre_serialize_message(obj)
+
+    async def deserialize_original_message(self, obj: Dict[str, Any]) -> OriginalMessage:
+        # TODO is it a bad idea to pop keys out of the original dictionary that was passed ?
+        msg_uuid = UUID(obj["uuid"])
+        sender_uuid = UUID(obj["sender"]["uuid"])
+        receiver_uuid = UUID(obj["receiver"]["uuid"])
+        prev_msg_uuid = UUID(obj["previous_message"]["uuid"]) if obj.get("previous_message") else None
+        requesting_msg_uuid = UUID(obj["requesting_message"]["uuid"]) if obj.get("requesting_message") else None
+        parent_ctx_msg_uuid = UUID(obj["parent_context"]["uuid"]) if obj.get("parent_context") else None
+        return OriginalMessage(
+            uuid=msg_uuid,
+            # TODO sender=await bot_merger.get_...(sender_uuid),
+            # TODO TODO TODO
+        )
 
     async def serialize_forwarded_message(self, obj: ForwardedMessage) -> Dict[str, Any]:
         result = await self._pre_serialize_message(obj)
